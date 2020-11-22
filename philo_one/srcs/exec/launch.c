8de505ebec9e6c361a37ca44af8a332ababdfa4b
Timothe Lecouvreur <timothelecouvreur@timothes-macbook-pro.home>
2020-11-22 16:08:09 +0100

    fix the launch problem

--- philo_one/srcs/exec/launch.c
+++ philo_one/srcs/exec/launch.c
@@ -6,46 +6,67 @@
 /*   By: timlecou <timlecou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/21 15:43:27 by timlecou          #+#    #+#             */
-/*   Updated: 2020/11/22 10:47:47 by timlecou         ###   ########.fr       */
+/*   Updated: 2020/11/22 16:07:03 by timlecou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_one.h"
 
-int	launch_philo(t_data *data)
+int	start_odd(t_data *data)
 {
-	unsigned int	 i;
+	unsigned int	i;
+	unsigned int	j;
 
 	i = 0;
+	j = 0;
+	while (i < data->ph_number / 2 + data->ph_number % 2)
+	{
+		data->index = j;
+		if (pthread_create(&data->ph[j].thread, NULL, start_routine, (void*)data) == -1)
+			return (EXIT_FAILURE);
+		if (pthread_create(&data->ph[j].death_thread, NULL, death_routine, (void*)data) == -1)
+			return (EXIT_FAILURE);
+		usleep(20);
+		pthread_detach(data->ph[j].thread);
+		pthread_detach(data->ph[j].death_thread);
+		++i;
+		j = j + 2;
+	}
+	return (EXIT_SUCCESS);
+}
+
+int	start_even(t_data *data)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	j = 1;
+	while (i < data->ph_number / 2)
+	{
+		data->index = j;
+		if (pthread_create(&data->ph[j].thread, NULL, start_routine, (void*)data) == -1)
+			return (EXIT_FAILURE);
+		if (pthread_create(&data->ph[j].death_thread, NULL, death_routine, (void*)data) == -1)
+			return (EXIT_FAILURE);
+		usleep(20);
+		pthread_detach(data->ph[j].thread);
+		pthread_detach(data->ph[j].death_thread);
+		++i;
+		j = j + 2;
+	}
+	return (EXIT_SUCCESS);
+}
+
+int	launch_philo(t_data *data)
+{
 	if (init_mutex(data) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
-	while (i < data->ph_number)
-	{
-		data->index = i;
-		if (pthread_create(&data->ph[i].thread, NULL, start_routine, (void*)data) == -1)
-			return (EXIT_FAILURE);
-		if (pthread_create(&data->ph[i].death_thread, NULL, death_routine, (void*)data) == -1)
-			return (EXIT_FAILURE);
-		usleep(20);
-		pthread_detach(data->ph[i].thread);
-		pthread_detach(data->ph[i].death_thread);
-		i = i + 2;
-	}
-	i = 1;
-	ft_usleep(5000);
-	while (i < data->ph_number)
-	{
-		data->index = i;
-		if (pthread_create(&data->ph[i].thread, NULL, start_routine, (void*)data) == -1)
-			return (EXIT_FAILURE);
-		if (pthread_create(&data->ph[i].death_thread, NULL, death_routine, (void*)data) == -1)
-			return (EXIT_FAILURE);
-		usleep(20);
-		pthread_detach(data->ph[i].thread);
-		pthread_detach(data->ph[i].death_thread);
-		i = i + 2;
-	}
-	//death_routine(data);
+	if (start_odd(data) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	//ft_usleep(5000);
+	if (start_even(data) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
 
